09a6f2d9046dc0b5fc27a5344a083e46cfcff0d7
HemGuruKapish <hemgurukapish@HemGuruKapishs-MacBook-Air.local>
2026-06-01 12:11:19 +0530

    1 June Homework

--- String_Combination.java
+++ String_Combination.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Main {
+class String_Combination {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter the string: ");
